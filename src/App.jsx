5f3c4e811ffc4567ddcd72a79fa2671923c9e7cc
MillerBarrera <mbarrerag@unal.edu.co>
2025-10-05 16:34:49 -0500

    Adding UserSection

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ import ReviewsSection from './components/ReviewsSection'
 import ProfessionalSection from './components/ProfessionalSection'
 import Footer from './components/Footer'
 import MainContent from './components/MainContent'
+import UserPage from './components/UserSection'
 import './styles/App.css'
 
-// Componente para la página de inicio
 const HomePage = () => (
   <>
     <HeroSection />
@@ -29,6 +29,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<MainContent />} />
+          <Route path="/user" element={<UserPage />} />
         </Routes>
       </main>
       <Footer />
